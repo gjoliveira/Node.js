c8d194d9b3dd0aff0227d003f9b906ca4a695d80
Gabriel José Oliveira <78929152+gjoliveira@users.noreply.github.com>
2022-08-21 10:29:59 -0300

    Aula 8

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ app.get('/projects', function (request, response) {
 	return response.json(['projeto 1', 'projeto 2']);
 });
 
+app.post('/projects', function (request, response) {
+	return response.json(['projeto 1', 'projeto 2', 'projeto 3']);
+});
+
+app.put('/projects/:id', function (request, response) {
+	return response.json(['projeto 1', 'projeto 2', 'projeto 3 atualizado...']);
+});
+
+app.delete('/projects/:id', function (request, response) {
+	return response.json(['projeto 1', 'projeto 2', 'projeto 3 deletado...']);
+});
+
 app.listen(3000, () => {
 	console.log('App is running on port 3000...');
 });
